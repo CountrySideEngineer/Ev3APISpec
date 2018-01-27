9d4fd16304652002059922eb774552172575c973
DeepSleepEngineer <deep-sleeper@nify.com>
2018-01-28 06:52:56 +0900

    Add files to test ev3_color_sensor_get_reflect function.

---
+++ copy_api_color_get_reflect_test.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+#Check whther the src file "api_color_get_ambient_test" exists or not.
+#If the file does not exist, exit.
+if [ ! -e "api_color_get_reflect_test" ]; then
+    echo "Application file api_color_get_reflect_test does not exist."
+    
+    exit
+fi
+
+#Check whther the destination directory, of EV3, "H:\ev3rt\apps".
+if [ ! -d /cygdrive/H/ev3rt/apps ]; then
+    echo "Ev3 device is not connected."
+    
+    exit
+fi
+
+#Copy application file to ev3 apps folder.
+cp ./api_color_get_reflect_test /cygdrive/H/ev3rt/apps
